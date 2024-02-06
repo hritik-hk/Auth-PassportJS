91029bde6da831407da9e0a1f8eccd8fa3f7caaa
hritik-hk <kumar.hritik4w@gmail.com>
2024-02-06 21:15:32 +0530

    added google strategy config, deleted .env

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,38 @@
-const passport = require('passport');
-const connection = require('./database');
-const {User} = require('../model/user');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const connection = require("./database");
+const { User } = require("../model/user");
 
-// TODO: passport.use();
+const options= {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback",
+  }
+
+module.exports = (passport) => {
+  passport.use(
+    new GoogleStrategy( options, async function (accessToken, refreshToken, profile, cb) {
+
+        try {
+            const user = await User.findone({_id: jwt_payload.sub},"_id email role");
+        
+            if (!user) {
+              return cb(null, false); //return null in error and false as user doesnt exist
+            }
+            else{
+                req.user = user // Assign the user object to req.user
+                return cb(null, user);
+            }
+        
+          } catch (err) {
+                return cb(err,false);
+          }
+
+
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    )
+  );
+};
